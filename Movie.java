e9c380df0552acba5202b15693651e51bd1c3a25
yanghui65 <398735000@qq.com>
2021-08-12 23:36:01 +0800

    [fix]: optimize movie and rental class

--- Movie.java
+++ Movie.java
@@ -25,5 +25,29 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+
+    public double getCharge(int dayRented){
+        double result = 0;
+
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (dayRented > 2) {
+                    result += (dayRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += dayRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (dayRented > 3) {
+                    result += (dayRented - 3) * 1.5;
+                    break;
+                }
+        }
+        return result;
+
+    }
 }
 
--- Rental.java
+++ Rental.java
@@ -18,27 +18,7 @@ public class Rental {
     }
 
     public double getCharge(){
-        double result = 0;
-
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDayRented() > 2) {
-                    result += (getDayRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDayRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (getDayRented() > 3) {
-                    result += (getDayRented() - 3) * 1.5;
-                    break;
-                }
-        }
-        return result;
-
+        return movie.getCharge(dayRented);
     }
     public int getFrequentRenterPoints() {
         if (getMovie().getPriceCode() == Movie.NEW_RELEASE
