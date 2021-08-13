a9c19d4056baa74ce050cee38bc8ab29cfcbdbfa
yanghui65 <398735000@qq.com>
2021-08-13 23:39:00 +0800

    [fix]: use Strategy , add Price class to optimize Movie class

--- Movie.java
+++ Movie.java
@@ -1,3 +1,4 @@
+import java.lang.IllegalArgumentException;
 public class Movie {
 
     public static final int CHILDREN = 2;
@@ -8,7 +9,7 @@ public class Movie {
 
     private String title;
 
-    private int priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode){
         this.title = title;
@@ -24,11 +25,23 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return priceCode;
+        return _price.getPriceCode();
     }
 
-    public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
+    public void setPriceCode(int arg ) {
+        switch(arg){
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDREN:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException( "Incorrent Price Code" );
+        }
     }
 
     public double getCharge(int dayRented){
